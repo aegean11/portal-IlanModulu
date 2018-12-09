fd699ebc9f3f6b240c65c2fa8a0c736b7bbd9a1a
aegean11 <ege.akman1@gmail.com>
2018-12-09 14:39:23 +0300

    first commit

---
+++ controllers/console/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\IlanModulu\controllers\console;
+
+
+/**
+ * Default controller for the `IlanModulu` module
+ */
+class DefaultController extends \kouosl\base\controllers\console\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
